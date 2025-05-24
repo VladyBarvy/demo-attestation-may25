0080d82e974d2cb4d73d4ed68fea49eea3f5c839
VladyBarvy <vladbarskoff@yandex.ru>
2025-05-24 16:15:31 +0300

    fix README.md #1

--- demo-exam/src/main/db.js
+++ demo-exam/src/main/db.js
@@ -3,8 +3,8 @@ import { Client } from 'pg';
 const connectionDataBase = async () => {
   const client = new Client({
     user: 'postgres',
-    password: '1234',
-    host: '172.28.32.1',  // 172.28.35.82  172.28.32.1
+    password: '1234',     // изменить на свой пароль
+    host: '172.28.32.1',  // изменить на свой localhost
     port: '5432',
     database: 'officedb',
   });
